2a1bb509725ba43c0296337978a6d23e08a7e569
KumarAkshayThakur <40594069+KumarAkshayThakur@users.noreply.github.com>
2018-06-26 11:46:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,20 +16,12 @@ def home():
 
 def rest_request_example():
     print (requests.get("http://140.86.15.104:3000/fighters/45/0/pink/KumarAkshayThakur").text)
-print (requests.get("http://140.86.15.104:3000/fighters/45/1/pink/KumarAkshayThakur").text)
-print (requests.get("http://140.86.15.104:3000/fighters/45/2/pink/KumarAkshayThakur").text)
-print (requests.get("http://140.86.15.104:3000/fighters/45/3/pink/KumarAkshayThakur").text)
-print (requests.get("http://140.86.15.104:3000/fighters/45/4/pink/KumarAkshayThakur").text)
-print (requests.get("http://140.86.15.104:3000/fighters/45/5/pink/KumarAkshayThakur").text)
-print (requests.get("http://140.86.15.104:3000/fighters/45/6/pink/KumarAkshayThakur").text)
-print (requests.get("http://140.86.15.104:3000/fighters/45/7/pink/KumarAkshayThakur").text)
-print (requests.get("http://140.86.15.104:3000/fighters/45/8/pink/KumarAkshayThakur").text)
-print (requests.get("http://140.86.15.104:3000/fighters/45/9/pink/KumarAkshayThakur").text)
+
 
 
 def read_db_SQL_example():
     conn = db.get_engine().connect()
-    sql = "SELECT * FROM SampleTable"
+    sql = "SELECT * FROM SecretTable"
     results = conn.execute(sql)
     rows = ""
     for row in results:
